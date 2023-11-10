70a5772fce807bfd16ef4580ddadd1354b7135f6
Mark Gross <mark.gross2001@gmail.com>
2023-11-10 15:17:02 -0800

    Properly parse trip update data

---
+++ mbta_data/TripUpdateParser/OutputRecord.cs
@@ -0,0 +1,3 @@
+namespace ConsoleApp1;
+
+public record OutputRecord(string trip_id, string stop_id, int stop_sequence, string? arrival, string? departure);
---
+++ mbta_data/TripUpdateParser/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using ConsoleApp1;
+using CsvHelper;
+
+var data = File.ReadAllText("/home/mark/school/math168/project/mbta_data/trip_updates.json");
+var updates = JsonSerializer.Deserialize<RootObject>(data);
+
+var output = new List<OutputRecord>();
+
+foreach (var e in updates!.entity.Where(e => e.trip_update.stop_time_update is not null))
+{
+    foreach (var u in e.trip_update.stop_time_update!)
+    {
+        var epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        // the timestamps are 19 hours off for some reason
+        epoch = epoch.AddHours(19);
+        DateTime? arrivalTimestamp = u.arrival is null ? null : epoch.AddSeconds(u.arrival.time);
+        DateTime? departureTimestamp = u.departure is null ? null : epoch.AddSeconds(u.departure.time);
+        var update = new OutputRecord(
+            e.trip_update.trip.trip_id,
+            u.stop_id,
+            u.stop_sequence,
+            arrivalTimestamp?.ToString("HH:mm:ss"),
+            departureTimestamp?.ToString("HH:mm:ss"));
+        output.Add(update);
+    }
+}
+
+using var sw = new StreamWriter("/home/mark/school/math168/project/mbta_data/stop_time_updates.csv");
+using var csvw = new CsvWriter(sw, CultureInfo.InvariantCulture);
+csvw.WriteRecords(output);
