82b458ffb4c2ad97b596c95b195c4f2ab6768052
Mark Gross <mark.gross2001@gmail.com>
2023-11-18 15:42:22 -0800

    Fix clean stop time output

--- Parsers/StopTimeCleaner/Program.cs
+++ Parsers/StopTimeCleaner/Program.cs
@@ -9,25 +9,26 @@ using var output = File.OpenWrite("/home/mark/school/math168/project/delay_data/
 using var writer = new StreamWriter(output);
 using var csvw = new CsvWriter(writer, CultureInfo.InvariantCulture);
 csvw.WriteHeader<CleanStopTime>();
+csvw.NextRecord();
 
 csvr.Read();
 csvr.ReadHeader();
 while (csvr.Read())
 {
     var stopTime = csvr.GetRecord<StopTime>()!;
-    
+
     var arrivalComponents = stopTime.ArrivalTime!.Split(":");
     var arrivalHours = int.Parse(arrivalComponents[0]);
     var arrivalMinutes = int.Parse(arrivalComponents[1]);
     var arrivalSeconds = int.Parse(arrivalComponents[2]);
     var arrivalSecondsSinceMidnight = 3600 * arrivalHours + 60 * arrivalMinutes + arrivalSeconds;
-    
+
     var departureComponents = stopTime.DepartureTime!.Split(":");
     var departureHours = int.Parse(departureComponents[0]);
     var departureMinutes = int.Parse(departureComponents[1]);
     var departureSeconds = int.Parse(departureComponents[2]);
     var departureSecondsSinceMidnight = 3600 * departureHours + 60 * departureMinutes + departureSeconds;
-    
+
     csvw.WriteRecord(new CleanStopTime
     {
         TripId = stopTime.TripId,
