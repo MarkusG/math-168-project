d817a3bb4bb16ec99c42aaead910182937f603c8
Mark Gross <mark.gross2001@gmail.com>
2023-11-21 14:55:37 -0800

    Port and rename query

--- delay_data/get_rapid_transit_delays_with_stop_names.sql
+++ delay_data/get_rapid_transit_delays_with_stop_names.sql
@@ -1,3 +1,11 @@
+.mode csv
+.headers on
+.import delays.csv delays
+.import ../Boston_MBTA_Data/stop_times.txt stop_times
+.import ../Boston_MBTA_Data/trips.txt trips
+.import ../Boston_MBTA_Data/routes.txt routes
+.import ../Boston_MBTA_Data/stops.txt stops
+
 select s.stop_name,delays.avg_delay from delays
 inner join stop_times st on st.stop_id = delays.stop_id
 inner join trips t on t.trip_id = st.trip_id
